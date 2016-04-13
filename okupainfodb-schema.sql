37a3aefbc94fa1898c9b00d2093597129cbe06c3
Max <max9@openmailbox.org>
2016-04-13 18:22:05 +0200

    Modificado
    Añadiso

--- okupainfodb-schema.sql
+++ okupainfodb-schema.sql
@@ -9,37 +9,44 @@ CREATE TABLE users (
 	password BINARY(16) NOT NULL,
 	email VARCHAR(255) NOT NULL,
 	fullname VARCHAR(255) NOT NULL,
-	description VARCHAR(512) NOT NULL,
+	description VARCHAR(512),
 	PRIMARY KEY (id)
 );
 
 CREATE TABLE casals(
-	id BINARY(16) NOT NULL,
-	loginid VARCHAR(15) NOT NULL UNIQUE,
-	password BINARY(16) NOT NULL,
-	email VARCHAR(255) NOT NULL,
-	fullname VARCHAR(255) NOT NULL,
-	description VARCHAR(512) NOT NULL,
-	valoracion FLOAT (16) NOT NULL, /* Positiva o Negativa según las votaciones*/
+	casalsid BINARY(16) NOT NULL,
+  adminid  BINARY(16) NOT NULL,
 	localization VARCHAR(264) NOT NULL, /*Donde se encuentra el casal (Pasaremos la dirección a la API de Google Maps que nos devolvera las coordenadas*/
 	latitud DOUBLE DEFAULT 0,
 	longitud DOUBLE DEFAULT 0,
-	PRIMARY KEY (id)
+  validado BOOLEAN NOT NULL DEFAULT FALSE,
+  email VARCHAR(255) NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  description VARCHAR(512),
+  FOREIGN KEY (adminid) REFERNCES users(id),
+	PRIMARY KEY (casalsid)
+);
+
+CREATE TABLE valoraciones_casals(
+  userid BINARY(16) NOT NULL,
+  casalsid BINARY(16) NOT NULL,
+  valoracion BOOLEAN NOT NULL
+  FOREIGN KEY (userid) REFERNCES users(id),
+  FOREIGN KEY (casalsid) REFERNCES casals(casalsid),
+  PRIMARY KEY (userid,casalsid)
 );
 
 CREATE TABLE events (
 	id BINARY(16) NOT NULL,
-	creatorid BINARY(16) NOT NULL,
+	casalsid BINARY(16) NOT NULL,
  	title VARCHAR(100) NOT NULL,
-	tipo VARCHAR(50) NOT NULL,
 	descripcion VARCHAR(500) NOT NULL,
-	valoracion VARCHAR(100),
 	localization VARCHAR(264)NOT NULL,
 	latitud DOUBLE DEFAULT 0,
 	longitud DOUBLE DEFAULT 0,
 	last_modified TIMESTAMP NOT NULL,
   	creation_timestamp DATETIME not null default current_timestamp,
-  	FOREIGN KEY (creatorid) REFERENCES casals(id),
+  	FOREIGN KEY (casalsid) REFERENCES casals(casalsid),
   	PRIMARY KEY (id)
 );
 
@@ -49,20 +56,6 @@ CREATE TABLE auth_tokens (
 	FOREIGN KEY (userid) REFERENCES users(id) on delete cascade,
 	PRIMARY KEY (token)
 );
-
-/*CREATE TABLE auth_tokensc (
-  casalid BINARY(16) NOT NULL,
-  token BINARY(16) NOT NULL,
-  FOREIGN KEY (casalid) REFERENCES casals(id) on delete cascade,
-  PRIMARY KEY (token)
-);
-
-CREATE TABLE casals_roles (
-	casalid BINARY(16) NOT NULL,
-	role ENUM ('admin', 'casal'),
-	FOREIGN KEY (casalid) REFERENCES casals(id) on delete cascade,
-	PRIMARY KEY (casalid,role)
-);*///Provisional a modo de prueba
 
 CREATE TABLE user_roles (
 	userid BINARY(16) NOT NULL,
@@ -85,7 +78,7 @@ CREATE TABLE comments_casals (
 	casalid BINARY (16) NOT NULL,
 	content VARCHAR(512) NOT NULL,
 	creation_timestamp DATETIME not null default current_timestamp,
-	FOREIGN KEY (casalid) REFERENCES casals(id),
+	FOREIGN KEY (casalid) REFERENCES casals(casalsid),
 	FOREIGN KEY (creatorid) REFERENCES users(id),
 	PRIMARY KEY(id)
 );
@@ -101,15 +94,22 @@ CREATE TABLE comments_events (
  	PRIMARY KEY(id)
 );
 
-insert into users (id, loginid, password, email, fullname, description) values (UNHEX(REPLACE(UUID(),'-','')), 'okupa', UNHEX(MD5('okupa')), 'okupa@info.com', 'okupainfo','okupa principal');
-select @userid:=id from users where loginid = 'okupa';
-insert into user_roles (userid, role) values (@userid, 'registered');
+insert into users (id, loginid, password, email, fullname, description) values (UNHEX(REPLACE(UUID(),'-','')), 'okupa', UNHEX(MD5('1234')), 'okupa@info.com', 'okupainfo','okupa principal');
+select @okupaid:=id from users where loginid = 'okupa';
+insert into user_roles (userid, role) values (@okupaid, 'registered');
 
-insert into casals (id, loginid, password, email, fullname, description, valoracion, localization, latitud, longitud) values (UNHEX(REPLACE(UUID(),'-','')), 'casal', UNHEX(MD5('casal')), 'casal@casal.com','supercasal', 'casal per a joves', +10, 'carrer antic', 32, 2);
+insert into users (id, loginid, password, email, fullname, description) values (UNHEX(REPLACE(UUID(),'-','')), 'spongebob', UNHEX(MD5('1234')), 'spongebob@info.com', 'Bob Esponja','okupa en Fondo Bikini');
+select @bobid:=id from users where loginid = 'spongebob';
+insert into user_roles (userid, role) values (@bobid, 'registered');
 
-select @creatorid:=id from casals where loginid = 'casal';
-insert into events (id, creatorid, title, tipo, descripcion, valoracion, localization, latitud, longitud, last_modified, creation_timestamp) values (UNHEX(REPLACE(UUID(),'-','')), @creatorid, 'quedada','barbacoa','mengem amb el amics',-1, 'carrer nou',42,1, '0000-00-00 00:00:00','NOW');
+insert into users (id, loginid, password, email, fullname, description) values (UNHEX(REPLACE(UUID(),'-','')), 'admin', UNHEX(MD5('1234')), 'admin@info.com', 'Administrador', null);
+select @adminid:=id from users where loginid = 'admin';
+insert into user_roles (userid, role) values (@adminid, 'admin');
 
-select @userid:=id from users where loginid = 'okupa';
-select @eventoid:=id from events where title = 'quedada';
-insert into users_events(userid, eventoid) values(@userid, @eventoid);
+insert into casals (casalsid, adminid, email, name, description, localization, latitud, longitud) values (UNHEX(REPLACE(UUID(),'-','')), @adminid, 'casal@casal.com', 'Casal Zeus', 'Te sentirás en el Olimpo', 'Passeig de Gràcia, la Dreta de l\'Eixample, Eixample, Barcelona, BCN, Catalonia, 08007, Spain', 41.3897264, 2.1679474);
+
+insert into casals (casalsid, adminid, email, name, description, localization, latitud, longitud, validado) values (UNHEX(REPLACE(UUID(),'-','')), @adminid, 'casal2@casal.com', 'Casal Sex Pistols', 'Anarchy in the UK', 'Avenida de Gràcia, la Dreta de l\'Eixample, Eixample, Barcelona, BCN, Catalonia, 08007, Spain', 41.3942409, 2.158823, true);
+
+select @casalsid:=id from casals where email = 'casal2@casal.com';
+insert into events (id, casalsid, title, descripcion, localization, latitud, longitud)
+values (UNHEX(REPLACE(UUID(),'-','')), @casalsid, 'Kedada fumeta','fumem amb el amics','Avenida de Gràcia, la Dreta de l\'Eixample, Eixample, Barcelona, BCN, Catalonia, 08007, Spain', 41.3942409, 2.158823);
