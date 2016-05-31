d3d5bdb0b227d0d6172191e47063fc1be5dbd0ba
lima9 <max9@openmailbox.org>
2016-05-31 14:28:35 +0200

    Improved timestamps and eventdates

--- okupainfodb-schema.sql
+++ okupainfodb-schema.sql
@@ -3,7 +3,7 @@ create database okupadb;
 
 use okupadb;
 
-CREATE TABLE users (
+CREATE TABLE users(
  	id BINARY(16) NOT NULL,
 	loginid VARCHAR(15) NOT NULL UNIQUE,
 	password BINARY(16) NOT NULL,
@@ -15,28 +15,28 @@ CREATE TABLE users (
 
 CREATE TABLE casals(
 	casalid BINARY(16) NOT NULL,
-  adminid  BINARY(16) NOT NULL,
+  	adminid  BINARY(16) NOT NULL,
 	localization VARCHAR(264) NOT NULL, /*Donde se encuentra el casal (Pasaremos la dirección a la API de Google Maps que nos devolvera las coordenadas*/
 	latitude DOUBLE DEFAULT 0,
 	longitude DOUBLE DEFAULT 0,
-  validado BOOLEAN NOT NULL DEFAULT FALSE,
-  email VARCHAR(255) NOT NULL UNIQUE,
-  name VARCHAR(255) NOT NULL,
-  description VARCHAR(512),
-  FOREIGN KEY (adminid) REFERENCES users(id),
+  	validado BOOLEAN NOT NULL DEFAULT FALSE,
+  	email VARCHAR(255) NOT NULL UNIQUE,
+  	name VARCHAR(255) NOT NULL,
+  	description VARCHAR(512),
+  	FOREIGN KEY (adminid) REFERENCES users(id),
 	PRIMARY KEY (casalid)
 );
 
 CREATE TABLE valoraciones_casals(
-  userid BINARY(16) NOT NULL,
-  casalid BINARY(16) NOT NULL,
-  valoracion BOOLEAN NOT NULL,
-  FOREIGN KEY (userid) REFERENCES users(id),
-  FOREIGN KEY (casalid) REFERENCES casals(casalid),
-  PRIMARY KEY (userid,casalid)
+  	userid BINARY(16) NOT NULL,
+  	casalid BINARY(16) NOT NULL,
+  	valoracion BOOLEAN NOT NULL,
+  	FOREIGN KEY (userid) REFERENCES users(id),
+  	FOREIGN KEY (casalid) REFERENCES casals(casalid),
+ 	PRIMARY KEY (userid,casalid)
 );
 
-CREATE TABLE events (
+CREATE TABLE events(
 	id BINARY(16) NOT NULL,
 	casalid BINARY(16) NOT NULL,
  	title VARCHAR(100) NOT NULL,
@@ -44,27 +44,27 @@ CREATE TABLE events (
 	localization VARCHAR(264)NOT NULL,
 	latitude DOUBLE DEFAULT 0,
 	longitude DOUBLE DEFAULT 0,
+	eventdate DATETIME NOT NULL,
 	last_modified TIMESTAMP NOT NULL,
 	creation_timestamp DATETIME not null default current_timestamp,
-  FOREIGN KEY (casalid) REFERENCES casals(casalid),
-  PRIMARY KEY (id)
+  	FOREIGN KEY (casalid) REFERENCES casals(casalid),
+  	PRIMARY KEY (id)
 );
 
 CREATE TABLE events_pictures (
-  eventid BINARY(16) NOT NULL,
-  userid BINARY(16) NOT NULL,
-  filename VARCHAR(16) NOT NULL,
-  FOREIGN KEY (userid) REFERENCES users(id) on delete cascade,
-  FOREIGN KEY (eventid) REFERENCES events(id) on delete cascade,
-  PRIMARY KEY (eventid,userid)
+  	eventid BINARY(16) NOT NULL,
+  	userid BINARY(16) NOT NULL,
+  	filename VARCHAR(16) NOT NULL,
+  	FOREIGN KEY (userid) REFERENCES users(id) on delete cascade,
+ 	FOREIGN KEY (eventid) REFERENCES events(id) on delete cascade,
+  	PRIMARY KEY (eventid,userid)
 );
 
 CREATE TABLE users_pictures (
-  userid BINARY(16) NOT NULL,
-  filename VARCHAR(16) NOT NULL,
-  FOREIGN KEY (userid) REFERENCES users(id) on delete cascade,
-  FOREIGN KEY (eventid) REFERENCES events(id) on delete cascade,
-  PR
+  	userid BINARY(16) NOT NULL,
+ 	filename VARCHAR(16) NOT NULL,
+  	FOREIGN KEY (userid) REFERENCES users(id) on delete cascade,
+  	PRIMARY KEY(userid)
 );
 
 CREATE TABLE auth_tokens (
@@ -94,6 +94,7 @@ CREATE TABLE comments_casals (
 	creatorid BINARY(16) NOT NULL,
 	casalid BINARY (16) NOT NULL,
 	content VARCHAR(512) NOT NULL,
+	last_modified TIMESTAMP NOT NULL,
 	creation_timestamp DATETIME not null default current_timestamp,
 	FOREIGN KEY (casalid) REFERENCES casals(casalid),
 	FOREIGN KEY (creatorid) REFERENCES users(id),
@@ -105,13 +106,14 @@ CREATE TABLE comments_events (
 	creatorid BINARY(16) NOT NULL,
 	eventoid BINARY(16) NOT NULL,
 	content VARCHAR(512) NOT NULL,
+	last_modified TIMESTAMP NOT NULL,
 	creation_timestamp DATETIME not null default current_timestamp,
 	FOREIGN KEY (creatorid) REFERENCES users(id),
 	FOREIGN KEY (eventoid) REFERENCES events(id),
  	PRIMARY KEY(id)
 );
 
-/*Mirar como crear Sting*/
+/*Rellenamos con ejemplos la base de datos*/
 
 insert into users (id, loginid, password, email, fullname, description) values (UNHEX(REPLACE(UUID(),'-','')), 'okupa', UNHEX(MD5('1234')), 'okupa@info.com', 'okupainfo','okupa principal');
 select @okupaid:=id from users where loginid = 'okupa';
@@ -130,4 +132,19 @@ insert into casals (casalid, adminid, email, name, description, localization, la
 insert into casals (casalid, adminid, email, name, description, localization, latitude, longitude, validado) values (UNHEX(REPLACE(UUID(),'-','')), @adminid, 'casal2@casal.com', 'Casal Sex Pistols', 'Anarchy in the UK', 'Avenida de Gràcia, la Dreta de l\'Eixample, Eixample, Barcelona, BCN, Catalonia, 08007, Spain', 41.3942409, 2.158823, true);
 
 select @casalid:=casalid from casals where email = 'casal2@casal.com';
-insert into events (id, casalid, title, description, localization, latitude, longitude) values (UNHEX(REPLACE(UUID(),'-','')), @casalid, 'Kedada fumeta','fumem amb el amics','Avenida de Gràcia, la Dreta de l\'Eixample, Eixample, Barcelona, BCN, Catalonia, 08007, Spain', 41.3942409, 2.158823);
+select @casalid1:=casalid from casals where email = 'casal@casal.com';
+insert into events (id, casalid, title, description, localization, latitude, longitude, eventdate) values (UNHEX(REPLACE(UUID(),'-','')), @casalid, 'Kedada fumeta','fumem amb el amics','Avenida de Gràcia, la Dreta de l\'Eixample, Eixample, Barcelona, BCN, Catalonia, 08007, Spain', 41.3942409, 2.158823,'2038-01-19 03:14:07');
+insert into events (id, casalid, title, description, localization, latitude, longitude, eventdate) values (UNHEX(REPLACE(UUID(),'-','')), @casalid1, 'Info fumeta','informem fumetes amb el amics','Avenida de Gràcia, la Dreta de l\'Eixample, Eixample, Barcelona, BCN, Catalonia, 08007, Spain', 41.3942409, 2.158823, '2020-01-19 03:14:07');
+
+select @eventoid:=id from events where title = 'Kedada fumeta';
+select @eventoid2:=id from events where title = 'Info fumeta';
+insert into users_events (userid, eventoid) values (@bobid,@eventoid);
+insert into users_events (userid, eventoid) values (@bobid,@eventoid2);
+
+select @creatorid:=id from users where loginid = 'spongebob';
+select @creatorid1:=id from users where loginid = 'okupa';
+insert into comments_events (id, creatorid, eventoid, content) values (UNHEX(REPLACE(UUID(),'-','')), @creatorid, @eventoid, 'Me parece super interesante eso');
+insert into comments_events (id, creatorid, eventoid, content) values (UNHEX(REPLACE(UUID(),'-','')), @creatorid1, @eventoid2, 'Me parece super interesante eso');
+insert into comments_casals (id, creatorid, casalid, content) values (UNHEX(REPLACE(UUID(),'-','')), @creatorid1, @casalid, 'Amazing this is for me');
+insert into comments_casals (id, creatorid, casalid, content) values (UNHEX(REPLACE(UUID(),'-','')), @creatorid, @casalid1, 'Amazing this is for me');
+
